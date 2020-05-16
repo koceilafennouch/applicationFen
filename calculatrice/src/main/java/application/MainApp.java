64aa7d80274c5948a093e0a07bf1fc2b2f695283
Fennouch Koceila <koceila.fennouch@etu.univ-orleans.fr>
2020-05-16 08:12:47 +0200

    tout vas bien tqtpas !

---
+++ calculatrice/src/main/java/application/MainApp.java
@@ -0,0 +1,11 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    primaryStage.show();
+    }
+}
